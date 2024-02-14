187c3f05284a1d013439031ce3176e4126ce4442
GirishCodeAlchemy <venkatareddygirish@gmail.com>
2024-02-13 23:28:59 -0500

    Added the named entity bert base ner model

---
+++ alchemy.py
@@ -0,0 +1,24 @@
+import json
+
+import requests
+
+
+class AlchemyAPI:
+    def __init__(self, hf_api_key, endpoint_url):
+        self.hf_api_key = hf_api_key
+        self.endpoint_url = endpoint_url
+
+    def invoke_model(self, inputs, parameters=None):
+        headers = {
+            "Authorization": f"Bearer {self.hf_api_key}",
+            "Content-Type": "application/json"
+        }
+        data = {"inputs": inputs}
+        if parameters is not None:
+            data.update({"parameters": parameters})
+        response = requests.post(
+            self.endpoint_url,
+            headers=headers,
+            data=json.dumps(data)
+        )
+        return json.loads(response.content.decode("utf-8"))
